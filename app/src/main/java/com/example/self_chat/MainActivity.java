eee431ac1c40ee8767cd047067b0d7b73d5aa634
Ofir Baltin <ofir.baltin@mail.huji.ac.il>
2019-04-06 21:27:26 +0300

    Finish

--- app/src/main/java/com/example/self_chat/MainActivity.java
+++ app/src/main/java/com/example/self_chat/MainActivity.java
@@ -1,14 +1,16 @@
 package com.example.self_chat;
 
-import android.os.PersistableBundle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String MESSAGE_VIEW_KEY = "message_view";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,17 +21,25 @@ public class MainActivity extends AppCompatActivity {
         final TextView messageOutput = findViewById(R.id.message_output);
         Button sendButton = findViewById(R.id.send_button);
 
+        if (savedInstanceState != null)
+        {
+            messageOutput.setText(savedInstanceState.getString(MESSAGE_VIEW_KEY));
+        }
+
         sendButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                messageOutput.setText(messageInput.getText());
-                messageInput.getText().clear();
+                Editable message = messageInput.getText();
+                messageOutput.setText(message.toString());
+                message.clear();
             }
         });
     }
 
     @Override
-    public void onSaveInstanceState(Bundle outState, PersistableBundle outPersistentState) {
-        super.onSaveInstanceState(outState, outPersistentState);
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        TextView messageOutput = findViewById(R.id.message_output);
+        outState.putString(MESSAGE_VIEW_KEY, messageOutput.getText().toString());
     }
 }
