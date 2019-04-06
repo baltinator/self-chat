4974c67087f3351d3e3f97317e710f5321a404e5
Ofir Baltin <ofir.baltin@mail.huji.ac.il>
2019-04-06 20:15:43 +0300

    Progress

--- app/src/main/java/com/example/self_chat/MainActivity.java
+++ app/src/main/java/com/example/self_chat/MainActivity.java
@@ -1,7 +1,12 @@
 package com.example.self_chat;
 
+import android.os.PersistableBundle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final EditText messageInput = findViewById(R.id.message_input);
+        final TextView messageOutput = findViewById(R.id.message_output);
+        Button sendButton = findViewById(R.id.send_button);
+
+        sendButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                messageOutput.setText(messageInput.getText());
+                messageInput.getText().clear();
+            }
+        });
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState, PersistableBundle outPersistentState) {
+        super.onSaveInstanceState(outState, outPersistentState);
     }
 }
